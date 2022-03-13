887ff45b16af92eafe503aab2c990755459b1fb6
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2022-03-13 19:27:29 +0100

    🔧 configure build setup for esbuild

--- esbuild.js
+++ esbuild.js
@@ -1,20 +1,47 @@
-const esbuild = require("esbuild");
-const process = require("process");
+import dotenv from "dotenv";
+import esbuild from "esbuild";
+import path from "path";
+import process from "process";
 
-const production = process.argv[2] === "production";
+dotenv.config();
+
+const flags = process.argv;
+
+const buildSettings = {
+  production: flags.includes("production"),
+};
+
+const testingVaultPath = path.join(
+  process.env.TESTING_VAULT_PATH,
+  ".obsidian",
+  "plugins",
+  "spectrum-companion"
+);
+
+const outSettings = buildSettings.production
+  ? { outfile: "main.js" }
+  : { outdir: testingVaultPath, outbase: "." };
+
+const entrySettings = buildSettings.production
+  ? [process.env.ENTRY_FILE]
+  : {
+      main: process.env.ENTRY_FILE,
+      styles: "styles.css",
+    };
 
 esbuild
   .build({
-    entryPoints: ["src/input.ts"],
+    entryPoints: entrySettings,
     bundle: true,
-    minify: false,
+    minify: buildSettings.production,
     external: ["obsidian"],
+    format: "cjs",
+    watch: !buildSettings.production,
     target: "es2016",
     logLevel: "info",
-    sourcemap: production ? false : "inline",
+    sourcemap: buildSettings.production ? false : "inline",
     treeShaking: true,
-    outfile: "main.js",
-    format: "cjs",
+    ...outSettings,
   })
   // eslint-disable-next-line unicorn/no-process-exit
   .catch(() => process.exit(1));
