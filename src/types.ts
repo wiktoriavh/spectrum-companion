3aad0886881d5bb825aced1720e3d2527e3c52d1
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2022-03-18 21:31:58 +0100

    🏷️ types for SliderSetting and CustomCommand w/ Hotkeys

--- src/types.ts
+++ src/types.ts
@@ -1,13 +1,22 @@
 export type PluginSettings = {
   scheme: Scheme;
   hue: number;
+  "side-docks": boolean;
 };
 
 export type PluginKeys = keyof PluginSettings;
 
 export type Scheme = "default-spectrum" | "monochrome-spectrum";
 
-export type CustomCommand = { id: string; name: string; key: PluginKeys };
+export type HotkeyModifiers = "Alt" | "Mod" | "Shift";
+
+export type CustomCommand = {
+  id: string;
+  name: string;
+  key: PluginKeys;
+  hotkeys?: [{ modifiers: HotkeyModifiers[]; key: string }];
+};
+
 export type CustomCommands = CustomCommand[];
 export type OnSelectCommand = (id: string) => void;
 
@@ -17,5 +26,18 @@ export type CustomSetting = {
   key: PluginKeys;
   id: string;
   options: CustomCommand[];
+  type: "dropdown" | "toggle";
 };
-export type CustomSettings = CustomSetting[];
+
+export type SliderSetting = {
+  name: string;
+  description: string;
+  key: PluginKeys;
+  id: string;
+  min: number;
+  max: number;
+  step: number;
+  type: "slider";
+};
+
+export type CustomSettings = (CustomSetting | SliderSetting)[];
