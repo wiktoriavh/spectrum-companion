b8d2473f423d374547c2b645c8690e687a395307
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2022-03-18 21:32:55 +0100

    ✨ function for updatin hue and toggle side docks

--- src/Plugin.ts
+++ src/Plugin.ts
@@ -3,7 +3,7 @@ import { Plugin } from "obsidian";
 
 import { CommandGenerator } from "./CommandGenerator";
 import { SpectrumSettingTab } from "./Settings";
-import { defaultSettings, schemes } from "./consts";
+import { defaultSettings, schemes, commands } from "./consts";
 import type { PluginSettings, PluginKeys } from "./types";
 
 // eslint-disable-next-line import/no-default-export
@@ -22,7 +22,7 @@ export default class SpectrumCompanion extends Plugin {
 
     this.addSettingTab(new SpectrumSettingTab(this.app, this));
 
-    this.generator.generateCommands(schemes);
+    this.generator.generateCommands(commands);
   }
 
   // eslint-disable-next-line @typescript-eslint/no-empty-function
@@ -43,18 +43,45 @@ export default class SpectrumCompanion extends Plugin {
     document.body.classList.add(this.settings.scheme);
   }
 
+  public updateHue(): void {
+    const { hue } = this.settings;
+    const { body } = document;
+
+    body.style.setProperty("--spectrum-hue", `${hue}`);
+  }
+
+  public toggleSideDocks(): void {
+    const sideDocks = this.settings["side-docks"];
+    const { body } = document;
+
+    body.style.setProperty(
+      "--spectrum-side-docks",
+      sideDocks ? "1 1 30px" : "1 1 0"
+    );
+  }
+
   public checkForUpdate(key: PluginKeys): void {
     switch (key) {
       case "scheme":
         this.updateScheme();
+        break;
+      case "hue":
+        this.updateHue();
+        break;
+      case "side-docks":
+        this.toggleSideDocks();
         break;
       default:
         break;
     }
   }
 
-  public changeSettingProperty(key: PluginKeys, value: string): void {
+  public changeSettingProperty(
+    key: PluginKeys,
+    value: PluginSettings[typeof key]
+  ): void {
     this.settings[key] = value;
+    this.saveSettings();
 
     this.checkForUpdate(key);
   }
