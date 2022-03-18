a66574edbc8a8734f02f982df69b0ddaff084a90
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2022-03-18 21:31:28 +0100

    ✨ write settings and commands

--- src/consts.ts
+++ src/consts.ts
@@ -3,6 +3,7 @@ import type { CustomCommands, CustomSettings, PluginSettings } from "./types";
 export const defaultSettings: PluginSettings = {
   scheme: "default-spectrum",
   hue: 0,
+  "side-docks": false,
 };
 
 export const schemes: CustomCommands = [
@@ -18,12 +19,50 @@ export const schemes: CustomCommands = [
   },
 ];
 
-export const schemeSettings: CustomSettings = [
+export const hue: CustomCommands = [
+  {
+    id: "change-hue",
+    name: "Change Hue",
+    key: "hue",
+  },
+];
+
+export const toggleRibbon: CustomCommands = [
+  {
+    id: "toggle-side-docks",
+    name: "Toggle Side Docks",
+    key: "side-docks",
+    hotkeys: [{ modifiers: ["Mod", "Alt"], key: "y" }],
+  },
+];
+
+export const commands = [...schemes, ...toggleRibbon];
+
+export const customSettings: CustomSettings = [
   {
     name: "Change Colour Scheme",
     description: "Change the colour scheme of the Spectrum Theme",
     id: "change-colour-scheme",
     key: "scheme",
     options: schemes,
+    type: "dropdown",
+  },
+  {
+    name: "Change Hue",
+    description: "Change the hue for the Spectrum Monochrome Theme",
+    id: "change-hue",
+    key: "hue",
+    type: "slider",
+    min: 0,
+    max: 360,
+    step: 1,
+  },
+  {
+    name: "Toggle Side Docks",
+    description: "Toggle the side docks",
+    id: "toggle-side-docks",
+    key: "side-docks",
+    options: toggleRibbon,
+    type: "toggle",
   },
 ];
