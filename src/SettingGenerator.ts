6cb830d8d3d2c0291957028ee77cecb40dddd293
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2022-03-18 21:33:41 +0100

    ✨ generator for Toggle and Slider

--- src/SettingGenerator.ts
+++ src/SettingGenerator.ts
@@ -1,13 +1,34 @@
 import { Setting } from "obsidian";
 
 import type SpectrumCompanion from "./Plugin";
-import type { CustomSetting, CustomSettings } from "./types";
+import type { CustomSetting, CustomSettings, SliderSetting } from "./types";
 
 export class SettingGenerator {
   public plugin: SpectrumCompanion;
 
   public constructor(plugin: SpectrumCompanion) {
     this.plugin = plugin;
+  }
+
+  public generateSettings(
+    settings: CustomSettings,
+    container: HTMLElement
+  ): void {
+    settings.forEach((setting) => {
+      switch (setting.type) {
+        case "dropdown":
+          this.generateDropdownSetting(setting, container);
+          break;
+        case "toggle":
+          this.generateToggleSetting(setting, container);
+          break;
+        case "slider":
+          this.generateSliderSetting(setting, container);
+          break;
+        default:
+          break;
+      }
+    });
   }
 
   public generateDropdownSetting(
@@ -34,19 +55,44 @@ export class SettingGenerator {
       });
   }
 
-  public generateSettings(
-    settings: CustomSettings,
-    typeOfSetting: string,
+  public generateToggleSetting(
+    settings: CustomSetting,
     container: HTMLElement
   ): void {
-    switch (typeOfSetting) {
-      case "dropdown":
-        settings.forEach((setting) => {
-          this.generateDropdownSetting(setting, container);
+    new Setting(container)
+      .setName(settings.name)
+      .setDesc(settings.description)
+      .addToggle((toggle) => {
+        const customToggle = toggle;
+
+        customToggle.setValue(this.plugin.settings[settings.key]);
+        customToggle.onChange(async (value) => {
+          this.plugin.changeSettingProperty(settings.key, value);
+          await this.plugin.saveSettings();
         });
-        break;
-      default:
-        break;
-    }
+
+        return customToggle;
+      });
+  }
+
+  public generateSliderSetting(
+    setting: SliderSetting,
+    container: HTMLElement
+  ): void {
+    new Setting(container)
+      .setName(setting.name)
+      .setDesc(setting.description)
+      .addSlider((slider) => {
+        const customSlider = slider;
+
+        customSlider.setValue(this.plugin.settings[setting.key]);
+        customSlider.setLimits(setting.min, setting.max, setting.step);
+        customSlider.onChange(async (value) => {
+          this.plugin.changeSettingProperty(setting.key, value);
+          await this.plugin.saveSettings();
+        });
+
+        return customSlider;
+      });
   }
 }
